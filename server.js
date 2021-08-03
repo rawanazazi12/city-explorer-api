61a1664b5fcf257be137a6c574fb76a0435a3e34
rawanazazi12 <rawanazazi12@gmail.com>
2021-08-03 22:49:51 +0300

    backend lab8

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express()
 const PORT = process.env.PORT;
-// const axios = require('axios');
+const axios = require('axios');
 app.use(cors());
 
 
